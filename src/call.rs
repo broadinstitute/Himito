68f6e5d05c41c39333056b68067b79d49847d3b7
hangsuUNC <suhang@broadinstitute.org>
2026-02-23 13:06:46 -0500

    revision

--- src/call.rs
+++ src/call.rs
@@ -996,7 +996,7 @@ pub fn resolve_thresholds(
     frequency_threshold: Option<f64>,
 ) -> (f64, f64) {
     let (default_p, default_f) = if data_type == "ont" {
-        (0.0001, 0.3)
+        (0.001, 0.3)
     } else {
         (0.001, 0.5)
     };
